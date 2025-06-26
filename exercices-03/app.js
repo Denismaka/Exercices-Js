0e9593364602fc0fe3cca381f04f5399b1a516b1
Denismaka <makadenis370@gmail.com>
2025-06-26 22:24:36 +0100

    Mise en place de la deuxième de l'exercices numeros 3

--- exercices-03/app.js
+++ exercices-03/app.js
@@ -75,3 +75,130 @@ students.forEach((student) => {
         `${student.firstname} ${student.lastname} ${result} avec une moyenne de : ${average}`
     );
 });
+
+// Notion des prototypes et des classes et la programmation orientée objet en JavaScript
+// ##########################################################
+// Exercice 3[1] : On traivaille sur un site qui utilise des
+// formes géométriques pour représenter des formes
+// ##########################################################
+
+// Classe de base pour les formes géométriques
+// La classe Retangle représente un rectangle avec une largeur et une hauteur
+class Retangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    get perimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    get isValid() {
+        return this.width > 0 && this.height > 0;
+    }
+
+    isBiggerThan(shape) {
+        return this.perimeter > shape.perimeter;
+    }
+    isBiggerThan(other) {
+        return this.perimeter > other.perimeter;
+    }
+}
+
+// Classe qui hérite de la classe Retangle pour représenter un carré
+// Un carré est un rectangle avec une largeur et une hauteur égales
+class Square extends Retangle {
+    constructor(width) {
+        super(width, width);
+    }
+}
+
+// Classe de base pour les formes géométriques
+const r = new Retangle(10, 20);
+console.log(r.perimeter);
+console.log(r.isValid);
+
+// Classe Rectangle qui hérite de la classe Forme
+const r2 = new Retangle(-10, 20);
+console.log(r.isValid);
+
+// Classe Forme
+const c = new Square(10);
+console.log(c.perimeter);
+console.log(c.isBiggerThan(r));
+
+
+
+// Notion des prototypes et des classes et la programmation orientée objet en JavaScript
+// ##########################################################
+// Exercice 3[2] : On va gérer une librairie avec des objets
+// ##########################################################
+
+class Book {
+    #page = 1;
+
+    constructor(title, pages) {
+        this.title = title;
+        this.pages = pages;
+    }
+
+    get page() {
+        return this.#page;
+    }
+
+    nextPage() {
+        if (this.#page < this.pages) {
+            this.#page++;
+        }
+    }
+
+    close() {
+        this.#page = 1;
+    }
+}
+
+class Library {
+    #books = [];
+
+    addBook(book) {
+        this.#books.push(book);
+    }
+
+    addBooks(books) {
+        for (let book of books) {
+            this.addBook(book);
+        }
+    }
+
+    findBooksByLetter(letter) {
+        const found = [];
+        for (let book of this.#books) {
+            if (
+                book.title[0].toLocaleLowerCase() === letter.toLocaleLowerCase()
+            ) {
+                found.push(book);
+            }
+        }
+        return found;
+    }
+}
+
+const b = new Book("Le Seigneur des Anneaux", 1000);
+console.log(b.page);
+b.nextPage();
+console.log(b.page);
+b.close();
+console.log(b.page);
+
+const library1 = new Library();
+library1.addBook(b);
+library1.addBooks([
+    new Book("Ready Player One", 100),
+    new Book("Oui-Oui", 10),
+    new Book("Sillage", 50),
+]);
+
+// Recherche des livres commençant par la lettre "S"
+const foundBooks = library1.findBooksByLetter("S");
+console.log(foundBooks);
