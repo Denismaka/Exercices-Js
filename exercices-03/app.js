124bde1fbaea62619594eb3cd50316bdd8d268a3
Denismaka <makadenis370@gmail.com>
2025-06-26 10:06:19 +0100

    Résolution de l'exercice

---
+++ exercices-03/app.js
@@ -0,0 +1,61 @@
+// Notion des prototypes et des classes et la programmation orientée objet en JavaScript
+// ##########################################################
+// Exercice 3 : Calculer la moyenne des notes d'un étudiant
+// ##########################################################
+const moyenne = (notes) => {
+    let sum = 0;
+    for (let note of notes) {
+        sum += note;
+    }
+    return sum / notes.length;
+};
+
+// Classe étudiant
+class Student {
+    ecole = "collège Saint Pierre";
+
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.notes = [];
+    }
+
+    setNotes(notes) {
+        this.notes = notes;
+    }
+
+    // Méthode pour calculer la moyenne des notes
+    getAverage() {
+        if (this.notes && this.notes.length > 0) {
+            return this.notes.reduce((a, b) => a + b, 0) / this.notes.length;
+        }
+        return 0;
+    }
+
+    // Méthode pour vérifier si l'étudiant peut réussir
+    canPass() {
+        return this.getAverage() >= 10;
+    }
+}
+
+// Création d'instances de la classe étudiant
+const denis = new Student("Denis", "Maka");
+const wilson = new Student("Wilson", "Maka");
+const gedeon = new Student("Gedeon", "Maka");
+const tony = new Student("Tony", "Maka");
+
+// Définition des notes pour chaque étudiant
+denis.setNotes([18, 19, 20, 17, 16]);
+wilson.setNotes([12, 14, 15, 13, 11]);
+gedeon.setNotes([10, 12, 13, 12, 10]);
+tony.setNotes([3, 7, 8, 6, 11]);
+
+// Affichage de la moyenne de chaque étudiant
+const students = [denis, wilson, gedeon, tony];
+students.forEach((student) => {
+    const average = student.getAverage();
+    const result = student.canPass() ? "a réussi" : "a échoué";
+    console.log(
+        `${student.firstname} ${student.lastname} ${result} avec une moyenne de : ${average}`
+    );
+});
