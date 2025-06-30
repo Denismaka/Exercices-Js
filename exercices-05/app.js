c351099503274a1981699e8b00ce37426c598f12
Denismaka <makadenis370@gmail.com>
2025-06-30 22:22:25 +0100

    Ajout d'une exemple concret

--- exercices-05/app.js
+++ exercices-05/app.js
@@ -2,13 +2,15 @@
 // Les timers
 // #############################################
 function decompte(n) {
-    console.log(n);
+    console.log(n); // Affiche le nombre actuel du compte à rebours
     if (n === 0) {
-        return;
+        console.log("Lancement du produit !");
+        return; // Fin du compte à rebours
     }
     setTimeout(() => {
-        decompte(n - 1);
+        decompte(n - 1); // Appel récursif après 1 seconde
     }, 1000);
 }
 
+// Démarrer le compte à rebours à partir de 3 secondes
 decompte(3);
