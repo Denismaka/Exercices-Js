8e46b05522899d7742770bcd424380945340e514
Denismaka <makadenis370@gmail.com>
2025-07-05 19:49:38 +0100

    Ajout du fichier js

---
+++ exercices-07/app.js
@@ -0,0 +1,24 @@
+// #############################################
+// système de modules
+// #############################################
+
+export function calculatrice(a, b, operation) {
+    switch (operation) {
+        case "+":
+            return addition(a, b);
+        case "-":
+            return soustraction(a, b);
+        case "*":
+            return multiplication(a, b);
+        case "/":
+            try {
+                return division(a, b);
+            } catch (e) {
+                return e.message;
+            }
+        default:
+            return "Opération non reconnue";
+    }
+}
+
+export const som = (items) => items.reduce((acc, item) => acc + item, 0);
