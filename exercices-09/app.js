ddb7af193d282faf1117ce5c1991d49c8ada6d74
Denismaka <makadenis370@gmail.com>
2025-07-11 19:41:16 +0100

    Mise en place de l'enoncer de l'exercice sur la manipulation du DOM

--- exercices-09/app.js
+++ exercices-09/app.js
@@ -1,6 +1,12 @@
 // #############################################
 // Manipuler le DOM
 // #############################################
+// Ce code charge dynamiquement des articles depuis une API (JSONPlaceholder) et les affiche dans le DOM. Il inclut :
+// Un loader pendant le chargement,
+// Une gestion d'erreur avec message,
+// La création de composants HTML pour chaque article.
+// Fonctionnel et clair, idéal pour afficher du contenu dynamique.
+
 /**
  * Crée un élément HTML répresentant un articles
  * @param {title: string, body: string} post
