977ee6e1a5b0e8642a6f4ff41ddd5992abcf0860
Denismaka <makadenis370@gmail.com>
2025-07-11 19:32:57 +0100

    feat: Chargement d'articles via API + gestion erreur

--- exercices-09/app.js
+++ exercices-09/app.js
@@ -1,3 +1,46 @@
 // #############################################
 // Manipuler le DOM
 // #############################################
+/**
+ * Crée un élément HTML répresentant un articles
+ * @param {title: string, body: string} post
+ * @return {HTMLlement}
+ */
+function createArticles(post) {
+    const article = document.createElement(article);
+    article.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+    `;
+    return article;
+}
+
+async function main() {
+    const wrapper = document.querySelector("#lastPost");
+    const loader = document.createElement("p");
+    loader.innerText = "Chargement...";
+    wrapper.append(loader);
+
+    try {
+        const r = await fetch("https://jsonplaceholder.typicode.com/users", {
+            headers: {
+                Accept: "application/json",
+            },
+        });
+
+        if (!r.ok) {
+            throw new Error("Erreur serveur");
+        }
+        const posts = await r.json();
+        loader.remove();
+        for (let post of posts) {
+            wrapper.append(createArticles(post));
+        }
+    } catch (error) {
+        loader.innerText = "Impossible de charger les articles";
+        loader.style.color = "red";
+        return;
+    }
+}
+
+main();
